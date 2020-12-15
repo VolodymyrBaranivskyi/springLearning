be714d904f0e12be209a160356182959acec72b4
Volodymyr Baranivskyi <yamahafz6n@bigmir.net>
2020-12-15 20:05:52 +0200

    Initial commit

---
+++ src/test/java/com/example/spring/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
